9f81fc6587b72403ca9a7d0e3375a9f1e90d4c1a
Nadir Hujdur <nadir.hujdur2002@gmail.com>
2021-03-07 21:24:51 +0100

    Update

---
+++ coding_challenge_9_4.js
@@ -0,0 +1,17 @@
+function camelCase(...names) {
+    // const wordEdited = word.trim().toLowerCase().split('_');
+    // const wordFinal = [];
+
+    // for (const item of wordEdited) 
+    //     wordFinal.push(item[0].toUpperCase() + item.slice(1));
+
+    // return wordFinal[0] + wordFinal[1];
+
+    const editedNames = [];
+    const finalNames = [];
+
+    console.log(editedNames);
+
+}
+
+camelCase('naDir_hujdur', 'NaiD_Hujdur');
